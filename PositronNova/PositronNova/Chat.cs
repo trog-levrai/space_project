633f2cfa28e8fa0ebe39cb415dd538605a179369
trog <ilan.dubois@epita.fr>
2014-01-18 21:36:17 +0100

    Texte dans une file, il ne reste plus qu'à l'afficher

--- PositronNova/PositronNova/Chat.cs
+++ PositronNova/PositronNova/Chat.cs
@@ -1,15 +1,41 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace PositronNova
 {
     class Chat
     {
+        //Capte le texte
+        //Le code sera à compléter pour l'implémentation de l'autocomplete pour shell
+        private bool tab = false;
+        private string input;
+        public void KBInput(KeyboardState keyboardState)
+        {
+            if (tab)
+            {
+                if (keyboardState.IsKeyDown(Keys.Enter))
+                {
+                    texto.Enqueue(input);
+                    input = "";
+                }
+                else
+                {
+                    input += keyboardState.ToString();
+                }
+            }
+            else
+            {
+                tab = keyboardState.IsKeyDown(Keys.Tab);
+            }
+        }
         //File du texte à afficher
-        private string[] texts = new string[] {"hello", "world", "!", "zboub"};
+        private Queue<string> texto = new Queue<string>(10);
+        private string[] texts = new string[10];
         //Position de l'affichage
         private Vector2 position;
         public Vector2 GetPosition()
@@ -19,6 +45,7 @@ namespace PositronNova
         }
         public string ReturnString()
         {
+            texto.CopyTo(texts, 0);
             string ans = "";
             foreach (string text in texts)
             {
@@ -26,5 +53,6 @@ namespace PositronNova
             }
             return ans;
         }
+
     }
 }
