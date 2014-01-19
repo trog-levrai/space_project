c14cb8ee010640d5f1ebef5f96c5c08adfbb8a43
trog <ilan.dubois@epita.fr>
2014-01-19 12:43:13 +0100

    Plus propre mais problème saut de ligne, lettres en trop et plus de 10 messages

--- PositronNova/PositronNova/Chat.cs
+++ PositronNova/PositronNova/Chat.cs
@@ -13,43 +13,22 @@ namespace PositronNova
         //Capte le texte
         //Le code sera à compléter pour l'implémentation de l'autocomplete pour shell
         private bool tab = false;
-        private Keys[] prevPressedKeys;
-        private string input;
+        private string input = "";
+        private KBInput kb = new KBInput();
         public void KBInput(KeyboardState keyboardState)
         {
             if (tab)
             {
-                Keys[] pressedKeys = keyboardState.GetPressedKeys();  
-                bool shift = keyboardState.IsKeyDown(Keys.LeftShift) || keyboardState.IsKeyDown(Keys.RightShift);
                 if (keyboardState.IsKeyDown(Keys.Enter))
                 {
                     texto.Enqueue(input);
                     input = "";
+                    kb.current = "";
                     tab = false;
-                    return;
                 }
-                //Gestion déguelasse mais obligatoire du clavier
                 else
                 {
-                    if (keyboardState.IsKeyDown(Keys.Space))
-                    {
-                        input += " ";
-                    }
-                    foreach (Keys key in pressedKeys)
-                    {
-                        if (!prevPressedKeys.Contains(key))
-                        {
-                            string keyString = key.ToString();
-                            if (keyString.Length == 1)
-                            {
-                                char c = keyString[0];
-                                if (!shift)
-                                    c += (char)('a' - 'A');
-                                input += "" + c;
-                            }
-                        }
-                    }
-                    prevPressedKeys = pressedKeys;
+                    input += kb.GetString(keyboardState);
                 }
             }
             else
@@ -59,22 +38,22 @@ namespace PositronNova
         }
         //File du texte à afficher
         private Queue<string> texto = new Queue<string>(10);
-        private string[] texts = new string[10];
         //Position de l'affichage
         private Vector2 position;
         public Vector2 GetPosition()
         {
-            position = new Vector2(0, GraphicsDeviceManager.DefaultBackBufferHeight - 12*texts.Length);
+            position = new Vector2(0, GraphicsDeviceManager.DefaultBackBufferHeight - 12*texto.LongCount());
             return position;
         }
         public string ReturnString(KeyboardState kb)
         {
-            texto.CopyTo(texts, 0);
+            string[] texts = new string[texto.Count];
+            texts = texto.ToArray();
             string ans = "";
-            foreach (string text in texts)
+            for (int i = 0; i < texts.Length; i++)
             {
-                if (text != "")
-                {ans = ans + text + "\n";}
+                if (texts[i] != "")
+                { ans = ans + texts[i] + "\n"; }
             }
             return ans;
         }
---
+++ PositronNova/PositronNova/KBInput.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Input;
+
+namespace PositronNova
+{
+    class KBInput
+    {
+        private Keys[] lastPressedKeys;
+        public string current;
+        private bool shift;
+        public KBInput()
+        {
+            lastPressedKeys = new Keys[0];
+            current = "";
+            shift = false;
+        }
+        public string GetString(KeyboardState kbState)
+        {
+            Keys[] pressedKeys = kbState.GetPressedKeys();
+            shift = kbState.IsKeyDown(Keys.RightShift) || kbState.IsKeyDown(Keys.LeftShift);
+            //Si la touche vient d'être relevée
+            foreach (Keys key in lastPressedKeys)
+            {
+                if (!pressedKeys.Contains(key))
+                    OnKeyUp(key);
+            }
+            //Si la touche était deja pressée
+            foreach (Keys key in pressedKeys)
+            {
+                if (!lastPressedKeys.Contains(key))
+                    OnKeyDown(key);
+            }
+            lastPressedKeys = pressedKeys;
+            return current;
+        }
+        private void OnKeyDown(Keys key)
+        {
+            current = "";
+        }
+        private void OnKeyUp(Keys key)
+        {
+            if (key != Keys.Tab && key != Keys.Enter)
+            {
+                if (key == Keys.Space)
+                {
+                    current += " ";
+                }
+                if (key.ToString().Length == 1)
+                {
+                    char c = key.ToString()[0];
+                    if (!shift)
+                        c += (char)('a' - 'A');
+                    current += "" + c;
+                }
+            }
+            
+        }
+    }
+}
