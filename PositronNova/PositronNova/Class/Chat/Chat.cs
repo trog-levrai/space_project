40ae239ebb86c4f5e31575cf3532b0bbdfef331d
trog <ilan.dubois@epita.fr>
2014-06-09 15:11:40 +0200

    Classe client + connexion

---
+++ PositronNova/PositronNova/Class/Chat/Chat.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace PositronNova
+{
+    class Chat
+    {
+        //Capte le texte
+        //Le code sera à compléter pour l'implémentation de l'autocomplete pour shell
+        private MatchCollection foo;
+        private bool tab;
+        private int rank;
+        private string input;
+        Game game;
+        private Regex regex;
+        private KBInput kb;
+        private string[] texts;
+        public Chat(Game game)
+        {
+            regex = new Regex(@"[;(\-)pdosx]*");
+            tab = false;
+            rank = 0;
+            this.game = game;
+            input = "";
+            kb = new KBInput();
+            texts = new string[10] {"", "", "", "", "", "", "", "", "", ""};
+        }
+        public void addString(string mess)
+        {
+            if (mess != "")
+            {
+                //On remplace par des emoticones
+                regex.Replace(mess, "\u1F601");
+                int i = 0;
+                do
+                {
+                    i += 1;
+                } while (i < texts.Length && texts[i] != "");
+                if (i == texts.Length && mess != "")
+                {
+                    //On décale tout et on met mess à la dernière place car tout est remplis
+                    for (int j = 1; j < texts.Length; j++)
+                    {
+                        texts[j - 1] = texts[j];
+                    }
+                    texts[texts.Length - 1] = mess;
+                    rank = 10;
+                }
+                if (mess != "" && i < texts.Length)
+                {
+                    texts[i] = mess;
+                    rank += 1;
+                }
+            }
+        }
+        public void KBInput(KeyboardState keyboardState)
+        {
+            if (tab)
+            {
+                if (keyboardState.IsKeyDown(Keys.Enter))
+                {
+                    byte[] msg = Encoding.Default.GetBytes(input);
+                    UdpClient udpClient = new UdpClient();
+                    udpClient.Send(msg, msg.Length, "10.45.3.241", 1234);
+                    udpClient.Close();
+                    addString(input);
+                    input = "";
+                    kb.current = "";
+                    tab = false;
+                }
+                else
+                {
+                    input += kb.GetString(keyboardState);
+                }
+            }
+            else
+            {
+                tab = keyboardState.IsKeyDown(Keys.Tab);
+            }
+        }
+        //Position de l'affichage
+        private Vector2 position;
+        public Vector2 GetPosition()
+        {
+            position = new Vector2(Camera2d.Origine.X, game.Window.ClientBounds.Height - 14*rank + Camera2d.Origine.Y);
+            return position;
+        }
+        //Génère le texte à afficher
+        public string ReturnString(KeyboardState kb)
+        {
+            string ans = "";
+            foreach (string text in texts)
+            {
+                if (text != "")
+                {
+                    if (text.StartsWith("f:"))
+                        ans += text.Substring(2) + " (Booo)\n";
+                    else
+                    {
+                        if (text.StartsWith("e:"))
+                            ans += text.Substring(2) + " (Yayy)\n";
+                        else
+                        {
+                            ans += "<" + text + ">\n";
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
+
+    }
+}
---
+++ PositronNova/PositronNova/Class/Client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PositronNova.Class
+{
+    class Client
+    {
+        public String name { get; private set; }
+        String host;
+        int port;
+        public Socket sock;
+        StreamReader clientReader;
+        StreamWriter clientWriter;
+        public Client(String name, String host, int port, Socket sock)
+        {
+            this.name = name;
+            this.host = host;
+            this.port = port;
+            this.sock = sock;
+            this.clientReader = new StreamReader(new NetworkStream(sock));
+            this.clientWriter = new StreamWriter(new NetworkStream(sock));
+        }
+        //Methode de connection au serveur
+        public void Connect(string nick)
+        {
+            sock.Connect("94.23.56.31", 1234);
+            if (Receive() == "accepted")
+            {
+                Send("nick");
+            }
+            else
+            {
+                System.Threading.Thread.Sleep(1000);
+                Connect(nick);
+            }
+        }
+        public void Send(string message)
+        {
+            clientWriter.WriteLine(message);
+            clientWriter.Flush();
+        }
+        public string Receive()
+        {
+            string message;
+            try
+            {
+                message = clientReader.ReadLine();
+                return message;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
