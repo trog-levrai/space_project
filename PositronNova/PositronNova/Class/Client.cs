79d4045ba54b4291f03f1c7b3bb175319a523429
trog <ilan.dubois@epita.fr>
2014-06-16 14:09:43 +0200

    Pas de deserialisation si null

--- PositronNova/PositronNova/Class/Client.cs
+++ PositronNova/PositronNova/Class/Client.cs
@@ -80,16 +80,19 @@ namespace PositronNova.Class
                     Unit.Unit[] units;
                     byte[] buffer = new byte[2048 * 128];
                     sock.Receive(buffer);
-                    MemoryStream mem = new MemoryStream(buffer);
-                mem.Seek(0, SeekOrigin.Begin);
-                    units = (Unit.Unit[])format.Deserialize(mem);
-                lock (enn)
+                if (buffer != null)
                 {
-                    enn.Clear();
-                    for (int i = 0; i < units.Length; i++)
+                    MemoryStream mem = new MemoryStream(buffer);
+                    mem.Seek(0, SeekOrigin.Begin);
+                    units = (Unit.Unit[]) format.Deserialize(mem);
+                    lock (enn)
                     {
-                        units[i].Friendly = false;
-                        enn.Add(units[i]);
+                        enn.Clear();
+                        for (int i = 0; i < units.Length; i++)
+                        {
+                            units[i].Friendly = false;
+                            enn.Add(units[i]);
+                        }
                     }
                 }
                 //}
